bca71f3d84f8388532ce55c4a934fe0a74288162
Tarjányi József <tarjanyi.jozsef@freemail.hu>
2022-02-10 22:06:12 +0100

    update

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading;
 
 namespace server
@@ -7,13 +8,25 @@ namespace server
     {
         static void Main(string[] args)
         {
+            ExeConfigurationFileMap customConfigFileMap = new ExeConfigurationFileMap();
+            customConfigFileMap.ExeConfigFilename = "config.xml";
+
+            Configuration customConfig = ConfigurationManager.OpenMappedExeConfiguration(customConfigFileMap, ConfigurationUserLevel.None);
+
+            AppSettingsSection appSettings = (customConfig.GetSection("appSettings") as AppSettingsSection);
+
+            string ip = appSettings.Settings["ip"].Value;
+            string port = appSettings.Settings["port"].Value;
+               
+
+
             Console.WriteLine("**** Server start ****");
 
                        
             Thread t = new Thread(delegate ()
             {
                 
-                Server myserver = new Server("192.168.8.253", 13000);
+                Server myserver = new Server(ip, Int32.Parse(port));
             });
             t.Start();
 
